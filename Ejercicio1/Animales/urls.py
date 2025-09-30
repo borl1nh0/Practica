b7380273b845c98ba7c332f47eaf72373c5aeabe
Juan Antonio Sancho <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-09-30 13:40:32 +0200

    Implement views and templates for Animal, Protectora, and Colaborador listings

--- Ejercicio1/Animales/urls.py
+++ Ejercicio1/Animales/urls.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render   
 from .models import Animal, Protectora , Colaborador
-from django.url import path  
+from django.urls import path  
 from .import views
 
 #Pagina de inicio
 def lista_animales(request):
     animales= Animal.objects.all()
-    return render(request, 'animales/lista_animales.html'), {'lista_animales':animales }
+    return render(request, 'animales/lista_animales.html', {'lista_animales':animales })
 
 def lista_protectoras(request):
     protectoras= Protectora.objects.all()
-    return render(request, 'animales/lista_protectoras.html'), {'lista_protectoras':protectoras }   
+    return render(request, 'animales/lista_protectoras.html', {'lista_protectoras':protectoras })   
 
 def lista_colaboradores(request):
     colaboradores= Colaborador.objects.all()
-    return render(request, 'animales/lista_colaboradores.html'), {'lista_colaboradores':colaboradores }
+    return render(request, 'animales/lista_colaboradores.html', {'lista_colaboradores':colaboradores })
+
 urlpatterns = [
+    path('', views.lista_animales, name='inicio'),
     path('animales/', views.lista_animales, name='lista_animales'),
     path('protectoras/', views.lista_protectoras, name='lista_protectoras'),
     path('colaboradores/', views.lista_colaboradores, name='lista_colaboradores'),
--- Ejercicio1/Animales/views.py
+++ Ejercicio1/Animales/views.py
@@ -5,12 +5,12 @@ from .models import Animal, Protectora, Colaborador
 
 def lista_animales(request):
     animales= Animal.objects.all()
-    return render(request, 'animales/lista_animales.html'), {'lista_animales':animales }
+    return render(request, 'animales/lista_animales.html', {'lista_animales':animales })
 
 def lista_protectoras(request):
     protectoras= Protectora.objects.all()
-    return render(request, 'animales/lista_protectoras.html'), {'lista_protectoras':protectoras }   
+    return render(request, 'animales/lista_protectoras.html', {'lista_protectoras':protectoras })  
 
 def lista_colaboradores(request):
     colaboradores= Colaborador.objects.all()
-    return render(request, 'animales/lista_colaboradores.html'), {'lista_colaboradores':colaboradores } 
+    return render(request, 'animales/lista_colaboradores.html', {'lista_colaboradores':colaboradores }) 
